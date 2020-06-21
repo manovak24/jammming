86fc01a20d014e8570a51cd5716dd1c7069ad778
emagdnimMQT <mark.w.novak@gmail.com>
2020-06-21 11:40:16 -0400

    add onKeyPress Enter to Playlist to save

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,20 +6,32 @@ class Playlist extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleNameChange = this.handleNameChange.bind(this);
+		this.save = this.save.bind(this);
 	}
 
 	handleNameChange(event) {
 		this.props.onNameChange(event.target.value);
 	}
+	
+	save() {
+		this.props.onSave();
+	}
 
 	render() {
 		return (
 			<div className="Playlist">
-				<input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
+				<input 
+				defaultValue={'New Playlist'} 
+				onChange={this.handleNameChange}
+				onKeyPress={event => {
+					if (event.key === "Enter") {
+						this.save();
+					}
+				}} />
 				<TrackList tracks={this.props.playlistTracks}
 					onRemove={this.props.onRemove}
 					isRemoval={true} />
-				<button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+				<button className="Playlist-save" onClick={this.save}>SAVE TO SPOTIFY</button>
 			</div>
 		)
 	}
