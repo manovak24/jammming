aab1f1dcbc2ee61d9c1a2cc900aa8902a40bea80
emagdnimMQT <mark.w.novak@gmail.com>
2021-01-03 13:10:14 -0500

    Updates

---
+++ precache-manifest.ad670e71a2c93a795f09d32ad2008d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e256c7a69bfe005463bb8d2085cac5",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "abc21a59c2ba863462bc",
+    "url": "/Jammming/static/css/main.7893c7aa.chunk.css"
+  },
+  {
+    "revision": "43bb5cec1544d9163fd4",
+    "url": "/Jammming/static/js/2.074bae49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jammming/static/js/2.074bae49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc21a59c2ba863462bc",
+    "url": "/Jammming/static/js/main.19176cfe.chunk.js"
+  },
+  {
+    "revision": "54abf7a0eeb8a1426608",
+    "url": "/Jammming/static/js/runtime-main.cd1c4b6a.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
